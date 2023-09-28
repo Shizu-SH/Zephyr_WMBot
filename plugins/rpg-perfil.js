64cef4248bdd4b7e9cd7e769839dc681bbed405c
Shizu-SH <119007576+Shizu-SH@users.noreply.github.com>
2023-09-28 19:53:05 +0100

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -1,28 +1,18 @@
 import {createHash} from 'crypto';
 import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
-
 const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
   let pp = 'https://telegra.ph/file/06cc652844ea19e8aed1c.jpg';
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  
-  // Verifica si el usuario está registrado en la base de datos de Greed Island
-  if (!(who in global.db.data.greedIslandUsers)) {
-    throw `El usuario mencionado no está registrado en Greed Island.`;
-  }
-
+  if (!(who in global.db.data.users)) throw `El usuario que está mencionando no está registrado en Greed Island`;
   try {
-    // Obtiene la imagen de perfil del usuario
     pp = await conn.profilePictureUrl(who);
   } catch (e) {
-    // En caso de error, se mantiene la imagen de perfil predeterminada
   } finally {
-    const {name, limit, lastclaim, registered, regTime, age, premiumTime} = global.db.data.greedIslandUsers[who];
+    const {name, limit, lastclaim, registered, regTime, age, premiumTime} = global.db.data.users[who];
     const username = conn.getName(who);
     const prem = global.prems.includes(who.split `@` [0]);
     const sn = createHash('md5').update(who).digest('hex');
-    
-    // Crea la cadena de texto del perfil de Greed Island
     const str = `*𝙽𝙾𝙼𝙱𝚁𝙴:* ${username} ${registered ? '(' + name + ') ': ''}
 *𝙽𝚄𝙼𝙴𝚁𝙾:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 *𝙻𝙸𝙽𝙺:* wa.me/${who.split`@`[0]}${registered ? '\n*𝙴𝙳𝙰𝙳:* ' + age + ' años' : ''}
@@ -31,13 +21,10 @@ const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
 *𝙿𝚁𝙴𝙼𝙸𝚄𝙼:* ${premiumTime > 0 ? 'Si' : (isPrems ? 'Si' : 'No') || ''}
 *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
 ${sn}`;
-    
-    // Envía la imagen de perfil y la información de Greed Island
     conn.sendMessage(m.chat, {image: {url: pp}, caption: str}, {quoted: m});
   }
 };
-
-handler.help = ['perfil [@usuario]'];
-handler.tags = ['greedisland'];
+handler.help = ['profile [@user]'];
+handler.tags = ['xp'];
 handler.command = /^perfil|profile?$/i;
 export default handler;
